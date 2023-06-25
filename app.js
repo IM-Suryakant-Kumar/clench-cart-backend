058702ec56cac04a5b166f026a4623e7e73a17d6
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-06-25 13:05:44 +0530

    feat: added authRouter and productsRouter

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ require("dotenv").config()
 const express = require("express")
 const app = express()
 
-// route
+// routers
+const productsRouter = require("./routes/products")
+const authRouter = require("./routes/auth")
+
 app.get("/", (req, res) => {
-    res.send("Clench Cart API")
+	res.send("Clench Cart API")
 })
+
+// routes
+app.use("api/v1/products", productsRouter)
+app.use("api/v1/auth", authRouter)
 
 const port = process.env.PORT || 3000
 
 const start = async () => {
 	try {
-		app.listen(port, console.log(`Server is listening on port ${port}`))
+		app.listen(port, () =>
+			console.log(`Server is listening on port ${port}`)
+		)
 	} catch (err) {
 		console.log(err)
 	}
