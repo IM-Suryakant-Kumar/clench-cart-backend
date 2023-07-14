ff255a6c11e0356f9ba873292398fab0b8d296ab
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-07-14 15:18:37 +0530

    feat: complement stripe route

--- routes/stripe.js
+++ routes/stripe.js
@@ -4,25 +4,84 @@ const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_KEY);
 
 router.post("/payment", async (req, res) => {
-	const session = await stripe.checkout.sessions.create({
-		line_items: [
-			{
-				price_data: {
-					currency: "inr",
-					product_data: {
-						name: "T-shirt"
-					},
-					unit_amount: 2000
+	const line_items = req.body.products.map((item) => {
+		return {
+			price_data: {
+				currency: "inr",
+				product_data: {
+					name: item.title,
+					images: [item.img],
+					description: item.desc,
+					metadata: {
+						id: item._id
+					}
 				},
-				quantity: 1
-			}
-		],
-		mode: "payment",
-		success_url: `${process.env.CLIENT_URL}/success`,
-		cancel_url: `${process.env.CLIENT_URL}/cart`
+				unit_amount: item.price * 100
+			},
+			quantity: item.quantity
+		};
 	});
 
-	res.send({ url: session.url });
+	try {
+		const session = await stripe.checkout.sessions.create({
+			shipping_address_collection: {
+				allowed_countries: ["US", "CA", "IN"]
+			},
+			shipping_options: [
+				{
+					shipping_rate_data: {
+						type: "fixed_amount",
+						fixed_amount: {
+							amount: 0,
+							currency: "inr"
+						},
+						display_name: "Free shipping",
+						delivery_estimate: {
+							minimum: {
+								unit: "business_day",
+								value: 5
+							},
+							maximum: {
+								unit: "business_day",
+								value: 7
+							}
+						}
+					}
+				},
+				{
+					shipping_rate_data: {
+						type: "fixed_amount",
+						fixed_amount: {
+							amount: 1500,
+							currency: "inr"
+						},
+						display_name: "Next day air",
+						delivery_estimate: {
+							minimum: {
+								unit: "business_day",
+								value: 1
+							},
+							maximum: {
+								unit: "business_day",
+								value: 1
+							}
+						}
+					}
+				}
+			],
+			phone_number_collection: {
+				enabled: true
+			},
+			line_items: line_items,
+			mode: "payment",
+			success_url: `${process.env.CLIENT_URL}/success`,
+			cancel_url: `${process.env.CLIENT_URL}/cart`
+		});
+
+		res.send({ url: session.url });
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
