565987595666c23839ecaa80ac5acc661445e078
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2024-02-17 11:44:40 +0530

    refactor: errors

--- errors/index.js
+++ errors/index.js
@@ -1,11 +1,40 @@
-const BadRequestError = require("./bad-request");
-const NotFoundError = require("./not-found");
-const UnauthenticatedError = require("./unauthenticated");
-const UnauthorizedError = require("./unauthorized");
+class CustomAPIError extends Error {
+	constructor(message) {
+		super(message);
+	}
+}
+
+class BadRequestError extends CustomAPIError {
+	constructor(message) {
+		super(message);
+		this.statusCode = StatusCodes.BAD_REQUEST;
+	}
+}
+
+class NotFoundError extends CustomAPIError {
+	constructor(message) {
+		super(message);
+		this.statusCodes = StatusCodes.NOT_FOUND;
+	}
+}
+
+class UnauthenticatedError extends CustomAPIError {
+	constructor(message) {
+		super(message);
+		this.statusCode = StatusCodes.UNAUTHORIZED;
+	}
+}
+
+class UnauthorizedError extends CustomAPIError {
+	constructor(message) {
+		super(message);
+		this.statusCode = StatusCodes.FORBIDDEN;
+	}
+}
 
 module.exports = {
 	BadRequestError,
 	NotFoundError,
 	UnauthenticatedError,
-	UnauthorizedError
+	UnauthorizedError,
 };
